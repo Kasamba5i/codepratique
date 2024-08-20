8837311511e270c4a095dc78eaf82cad73ddc414
Kasamba5i <141848316+Kasamba5i@users.noreply.github.com>
2024-08-20 21:55:20 +0100

    ecriture du code VUE

---
+++ App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const msg = ref('');
+</script>
+
+<template>
+    <main>
+        <h1>Playground</h1>
+        <InputText />
+        <p></p>
+    </main>
+</template>
